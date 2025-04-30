3474943e7a23c1c42339982aaba3e74ef8b516b1
Tina137 <108569035+Tina137@users.noreply.github.com>
2025-04-30 13:16:09 +0300

    Update faq.js

--- src/js/faq.js
+++ src/js/faq.js
@@ -2,7 +2,6 @@ import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 new Accordion('.accordion-container', {
   openOnInit: [0],
-  collapse: false,
   beforeOpen: currElement => {
     currElement.classList.add('open');
   },
