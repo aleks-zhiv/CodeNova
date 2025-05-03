2bb5a19d1f5d3e0084d60d4ede6178191593245f
Viktor <viktoryanenko11@gmail.com>
2025-05-03 20:49:08 +0300

    update fonts

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+     base: '/CodeNova-copy/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
